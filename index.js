56d8c951568fdde3c01faaafdadd525ffb2459aa
balaraju404 <balarajugandham1@gmail.com>
2024-05-17 14:25:44 +0530

    commit two

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const express = require('express');
+const mysql = require("mysql")
 
 const app = express();
 
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'balaraju'
+})
+
+conn.connect((error) => {
+    if (error) throw error;
+    console.log('Database Connected');
+})
+
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/users', (req, res) => {
+    conn.query('SELECT * FROM users', (error, results) => {
+        if (error) throw error;
+        res.send(results);
+    })
 })
 
 app.listen(3001, (error) => {
